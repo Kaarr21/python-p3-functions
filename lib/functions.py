e023cda3f52b7402183515ede1c49bc033380b1a
Karoki <wanjikukar19@gmail.com>
2025-05-05 18:27:58 +0300

    Functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(name = "programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+    #greet_with_default("Nancy")
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
+    return number/2
     pass
